7bf255cdbbbaaa88f0776591680dbb299cab909f
Hex <wolfgunmiller@gmail.com>
2020-02-28 12:08:05 +0300

    Singleton session. Image from url

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -13,8 +13,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var getImageButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
+    }
+
 
     @IBAction func getImagePressed(_ sender: Any) {
+        label.isHidden = true
+        getImageButton.isEnabled = false
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://images.unsplash.com/photo-1538464721630-06563f546140?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max") else {
+            activityIndicator.isHidden = true
+            label.text = "Error's recieved"
+            label.isHidden = false
+            return }
+        
+        URLSession.shared/* Singleton session */
+            .dataTask(with: url) { (data, response, error) in
+                if let data = data, let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.activityIndicator.stopAnimating()
+                        self.imageView.image = image
+                    }
+                }
+        }.resume()
     }
     
 }
