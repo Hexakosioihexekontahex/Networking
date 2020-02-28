c9aa3fa9472d29451f650a1875c44c725762143b
Hex <wolfgunmiller@gmail.com>
2020-02-28 11:44:41 +0300

    Initial Commit

---
+++ Networking/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Roman Melnik on 28/02/2020.
+//  Copyright © 2020 Roman Melnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var getImageButton: UIButton!
+
+    @IBAction func getImagePressed(_ sender: Any) {
+    }
+    
+}
+
