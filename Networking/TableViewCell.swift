9918469eef8a031030c7db3fc6642e1417d5c5fa
Hex <wolfgunmiller@gmail.com>
2020-02-28 15:30:29 +0300

    JSONDecoder

---
+++ Networking/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  Networking
+//
+//  Created by Roman Melnik on 28/02/2020.
+//  Copyright © 2020 Roman Melnik. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet var courseImage: UIImageView!
+    @IBOutlet var courseNameLabel: UILabel!
+    @IBOutlet var numberOfLessons: UILabel!
+    @IBOutlet var numberOfTests: UILabel!
+}
